ac639a889d388c9a09442fe32cb9b44d77b3c9ac
[Taiga <[iri.taiga@gmail.com]>
2023-10-02 08:37:01 +0900

    # This is a combination of 55 commits. # This is the 1st commit message:
    Ver1.0
    
    # The commit message #2 will be skipped:
    
    # ブランチ確認
    
    # The commit message #3 will be skipped:
    
    # gspreadの追加
    
    # The commit message #4 will be skipped:
    
    # シークレットの修正
    
    # The commit message #5 will be skipped:
    
    # フォーマット
    
    # The commit message #6 will be skipped:
    
    # さすがに修正
    
    # The commit message #7 will be skipped:
    
    # oauth2部分の修正
    
    # The commit message #8 will be skipped:
    
    # 修正？
    
    # The commit message #9 will be skipped:
    
    # デバッグ用
    
    # The commit message #10 will be skipped:
    
    # よくわからん
    
    # The commit message #11 will be skipped:
    
    # google.auth
    
    # The commit message #12 will be skipped:
    
    # auth
    
    # The commit message #13 will be skipped:
    
    # インストール？
    
    # The commit message #14 will be skipped:
    
    # line_chart
    
    # The commit message #15 will be skipped:
    
    # spreadsheet追加
    
    # The commit message #16 will be skipped:
    
    # submit_button
    
    # The commit message #17 will be skipped:
    
    # list_index
    
    # The commit message #18 will be skipped:
    
    # int_error
    
    # The commit message #19 will be skipped:
    
    # debug用
    
    # The commit message #20 will be skipped:
    
    # date_time
    
    # The commit message #21 will be skipped:
    
    # date_error
    
    # The commit message #22 will be skipped:
    
    # iso
    
    # The commit message #23 will be skipped:
    
    # date_val
    
    # The commit message #24 will be skipped:
    
    # debug
    
    # The commit message #25 will be skipped:
    
    # count_row
    
    # The commit message #26 will be skipped:
    
    # とりあえず
    
    # The commit message #27 will be skipped:
    
    # initial_commmit
    
    # The commit message #28 will be skipped:
    
    # tuple
    
    # The commit message #29 will be skipped:
    
    # no
    
    # The commit message #30 will be skipped:
    
    # cell_list
    
    # The commit message #31 will be skipped:
    
    # row
    
    # The commit message #32 will be skipped:
    
    # added_count
    
    # The commit message #33 will be skipped:
    
    # 結構変更
    
    # The commit message #34 will be skipped:
    
    # requirements
    
    # The commit message #35 will be skipped:
    
    # operate抜き
    
    # The commit message #36 will be skipped:
    
    # targetRow
    
    # The commit message #37 will be skipped:
    
    # 各々df
    
    # The commit message #38 will be skipped:
    
    # え
    
    # The commit message #39 will be skipped:
    
    # 合計
    
    # The commit message #40 will be skipped:
    
    # どうなる？
    
    # The commit message #41 will be skipped:
    
    # df生計
    
    # The commit message #42 will be skipped:
    
    # chart
    
    # The commit message #43 will be skipped:
    
    # expander
    
    # The commit message #44 will be skipped:
    
    # グラフ
    
    # The commit message #45 will be skipped:
    
    # pyplot
    
    # The commit message #46 will be skipped:
    
    # alatair_chart
    
    # The commit message #47 will be skipped:
    
    # 微修正
    
    # The commit message #48 will be skipped:
    
    # もう！
    
    # The commit message #49 will be skipped:
    
    # bargraph
    
    # The commit message #50 will be skipped:
    
    # 生計語
    
    # The commit message #51 will be skipped:
    
    # 折れ線
    
    # The commit message #52 will be skipped:
    
    # ウィジェット
    
    # The commit message #53 will be skipped:
    
    # でバグ
    
    # The commit message #54 will be skipped:
    
    # いい感じ
    
    # The commit message #55 will be skipped:
    
    # version1

--- main.py
+++ main.py
@@ -3,60 +3,173 @@ import numpy as np
 import pandas as pd
 import time
 
-st.title("Streamlit 超入門")
-
-st.write("DataFrame")
-
-df = pd.DataFrame({
-    "1列目": [1,2,3,4],
-    "2列目": [10,20,30,40]
-})
-
-if st.checkbox("Show Image"):
-    st.dataframe(df, width=100,height=400)
-    st.line_chart(df)
-
-text = st.text_input(
-    "あなたが好きな数字を教えてください")
-
-"あなたの好きな数字は、", text, "です"
-
-date = st.date_input("日付")
-st.sidebar.write("ウィジェット")
-left_column, right_column = st.columns(2)
-button = left_column.button("左カラム")
-text1 = right_column.text_input("右カラム")
-expander = st.expander("問い合わせ")
-expander.write("問い合わせ内容を書く")
-
-st.write("プログレスバー")
-"start!!"
-latest_iteration = st.empty()
-bar = st.progress(0)
-for i in range(100):
-    latest_iteration.text(f"iteration{i+1}")
-    bar.progress(i+1)
-    time.sleep(0.1)
-
+import gspread
+import pandas as pd
+from google.oauth2 import service_account
 from PIL import Image
- 
+import streamlit as st
+import matplotlib.pyplot as plt
+import seaborn as sns
+import altair as alt
+
+def count_rows(sheet):
+    values = sheet.get_all_values()
+    return len(values)
+
+def count_columns(sheet):
+    values = sheet.get_all_values()
+    if not values:
+        return 0  # シートにデータがない場合、0を返す
+
+    # 各行の列数を調べて最大の列数を取得
+    max_column_count = max(len(row) for row in values)
+    return max_column_count
+
+def copyDataToBudgetSheet(questions, sheet, lastRow = False):
+    if lastRow:
+        targetRow = count_rows(sheet)  # 家計簿シートに追加する行
+        for num, question in enumerate(questions):
+            question_category, answer = question[0], question[1]
+            sheet.update_cell(targetRow + 1, num+1, answer)
+    else:
+        targetRow = 0
+        for num, question in enumerate(questions):
+            question_category = question[0]
+            sheet.update_cell(targetRow + 1, num+1, question_category)
+    # フォームからのデータをリストとして受け取る
+
+question_categories = ["日付", "カテゴリ", "詳細", "収入", "支出"]
+
+# スプレッドシートの認証
+scopes = [ 'https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive'
+]
+credentials = service_account.Credentials.from_service_account_info( st.secrets["gcp_service_account"], scopes=scopes
+)
+gc = gspread.authorize(credentials)
+# スプレッドシートからデータ取得
+SHEET_KEY = st.secrets.SP_SHEET_KEY.key # スプレッドシートのキー
+sh = gc.open_by_key(SHEET_KEY)
+SP_SHEET = 'シート1' # シート名「シート1」を指定 # シート名「シート1」を指定
+worksheet = sh.worksheet(SP_SHEET)
+
+question_list = [[value] for value in question_categories]
+copyDataToBudgetSheet(question_list, worksheet)
+
+st.sidebar.write("""
+    ## データを見る
+""")
+st.sidebar.slider("ウィジェット",1,50,20)
+row_data_button = st.sidebar.checkbox("生データを見る")
+monthly_transition_button = st.sidebar.checkbox("資産推移を見る")
+monthly_category_button = st.sidebar.checkbox("月ごとの収支を見る")
+
 categories = ["給与", "二人で遊ぶお金", "食費/消耗品", "耐久消耗品", "お小遣い", "楽天証券前月比", "その他"]
+
 with st.form("my_form", clear_on_submit=True):
-    date = st.date_input("日付*")
-    category = st.selectbox(label='カテゴリ', options=categories)
-    description = st.text_area('詳細')
-    income = st.text_input("収入")
-    expense = st.text_input("支出")
+    date = st.date_input(question_categories[0])
+    category = st.selectbox(label=question_categories[1], options=categories)
+    description = st.text_area(question_categories[2])
+    income = st.text_input(question_categories[3])
+    expense = st.text_input(question_categories[4])
     submitted = st.form_submit_button("送信")
-     
-     
+
 if submitted:
-    with st.spinner("画像生成中です..."):
-        time.sleep(3)
-    income = int(income)
-    expense = int(expense)
-    # image = Image.open('キングスライム.jpg')
-    # st.subheader(name)
-    # st.image(image)
-    # st.text(description)  
-    # st.text(series)
+    with st.spinner("データ更新中..."):
+        time.sleep(1)
+    val = date.isoformat()
+    questions = [val, category, description, income, expense]
+    result = [[category, answer] for category, answer in zip(question_categories, questions)]
+    copyDataToBudgetSheet(result, worksheet, True)
+
+def get_dataFrame(sheet_key, sheet):
+    sh = gc.open_by_key(sheet_key)
+    
+    worksheet = sh.worksheet(sheet)
+    data = worksheet.get_all_values() # シート内の全データを取得
+    df = pd.DataFrame(data[1:], columns=data[0]) # 取得したデータをデータフレームに変換 
+    df["収入"] = df["収入"].replace('', '0').astype(int)
+    df["支出"] =df["支出"].replace('', '0').astype(int)
+
+    # 日付列を日付型に変換
+    df['日付'] = pd.to_datetime(df['日付'], format='ISO8601')
+
+    df['月'] = df['日付'].dt.strftime('%Y-%m')
+    df['収支'] = df['収入'] - df['支出']
+
+    return df
+
+if row_data_button:
+    st.dataframe(get_dataFrame(SHEET_KEY, SP_SHEET))
+
+if monthly_transition_button:
+    df = get_dataFrame(SHEET_KEY, SP_SHEET)
+    pivot_df = df.pivot_table(index='月', columns='カテゴリ', values='収支', aggfunc='sum', fill_value=0).reset_index()
+    #cat_monthly_summary = df.groupby(['月', 'カテゴリ'])['支出'].sum().reset_index()
+
+    # 月別合計収支を作りたい
+    monthly_total = pivot_df.groupby('月').sum()
+
+    # 合計列を追加
+    monthly_total['合計'] = monthly_total.sum(axis=1)
+
+    st.line_chart(monthly_total['合計'])
+
+if monthly_category_button:
+    df = get_dataFrame(SHEET_KEY, SP_SHEET)
+    # 月の選択用Expander
+    selected_month = st.selectbox("月を選択してください", df['月'].unique())
+
+    # 選択された月のデータをフィルタリング
+    filtered_df = df[df['月'] == selected_month]
+
+    # グラフのプロット
+    st.subheader(f"{selected_month}の各カテゴリーごとの収支")
+
+    # グラフのプロット用のデータを準備
+    category_summary = filtered_df.groupby('カテゴリ')['収支'].sum().reset_index()
+    # # 棒グラフのプロット
+    bars = (
+        alt.Chart(category_summary)
+        .mark_bar()
+        .encode(
+            x="カテゴリ:N",
+            y=alt.Y("収支:Q"),
+            color="カテゴリ:N",
+        )
+    )
+
+    st.altair_chart(bars, use_container_width=True)
+
+    # chart =(
+    #     alt.Chart(category_summary)
+    #     .mark_line(opacity=0.8)
+    #     .encode(
+    #         x="カテゴリ:N",
+    #         y=alt.Y("収支:Q", stack=None),
+    #         color="カテゴリ:N"
+    #     )
+    # )
+    # plt.figure(figsize=(10, 6))
+    # sns.barplot(x='カテゴリ', y='収支', data=category_summary)
+    # plt.xlabel('カテゴリ')
+    # plt.ylabel('収支')
+    # plt.xticks(rotation=45)
+    # plt.title(f"{selected_month}の各カテゴリーごとの収支")
+    # st.pyplot(plt)
+    #melt
+    #loc      
+
+
+
+    
+
+
+
+# st.write("プログレスバー")
+# "start!!"
+# latest_iteration = st.empty()
+# bar = st.progress(0)
+# for i in range(100):
+#     latest_iteration.text(f"iteration{i+1}")
+#     bar.progress(i+1)
+#     time.sleep(0.1)
