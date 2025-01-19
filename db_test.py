075ff0d2a77375f7a0f18dab5ae608df40651258
taigairisa <iri.taiga@gmail.com>
2025-01-19 23:59:18 +0900

    No code changes made

--- db_test.py
+++ db_test.py
@@ -2,7 +2,6 @@ from pathlib import Path
 import sqlite3
 from datetime import datetime
 import streamlit as st
-import altair as alt
 import pandas as pd
 from collections import defaultdict
 from google.oauth2 import service_account
@@ -12,6 +11,7 @@ import re
 SHEET_KEY = st.secrets.SP_SHEET_KEY.key
 SPREADSHEET_SCOPES = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
 SERVICE_ACCOUNT = st.secrets["gcp_service_account"]
+DB_FILENAME = Path(__file__).parent / "kakeibo.db"
 
 def get_worksheet_from_gspread_client():
     credentials = service_account.Credentials.from_service_account_info(SERVICE_ACCOUNT, scopes=SPREADSHEET_SCOPES)
@@ -19,46 +19,40 @@ def get_worksheet_from_gspread_client():
     sh = gc.open_by_key(SHEET_KEY)
     return sh
 
-sh = get_worksheet_from_gspread_client()
+def exists_db_file():
+    return DB_FILENAME.exists()
 
 def connect_db():
     """Connects to the sqlite database."""
-
-    DB_FILENAME = Path(__file__).parent / "kakeibo.db" #"inventory.db"
-    db_already_exists = DB_FILENAME.exists()
-
     conn = sqlite3.connect(DB_FILENAME)
-    db_was_just_created = not db_already_exists
+    return conn
 
-    return conn, db_was_just_created
-
-
-conn, db_was_just_created = connect_db()
-
-def initialize_data(conn):
+def initialize_data(conn, sh):
     """Initializes the inventory table with some data."""
     cursor = conn.cursor()
+    create_tables(cursor)
+    insert_initial_categories(cursor)
+    insert_initial_sub_categories(cursor)
+    insert_transactions_from_sheets(cursor, sh)
+    conn.commit()
+    conn.close()
 
-    cursor.execute(
-        """
+def create_tables(cursor):
+    cursor.execute("""
         CREATE TABLE IF NOT EXISTS main_categories (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT NOT NULL
         );
-        """
-    )
-    cursor.execute(
-        """
+    """)
+    cursor.execute("""
         CREATE TABLE IF NOT EXISTS sub_categories (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             main_category_id INTEGER NOT NULL,
             name TEXT NOT NULL,
             FOREIGN KEY (main_category_id) REFERENCES main_categories(id)
         );
-        """
-    )
-    cursor.execute(
-        """
+    """)
+    cursor.execute("""
         CREATE TABLE IF NOT EXISTS transactions (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             sub_category_id INTEGER NOT NULL,
@@ -68,134 +62,56 @@ def initialize_data(conn):
             detail TEXT,
             FOREIGN KEY (sub_category_id) REFERENCES sub_categories(id)
         );     
-        """
-    )
-       
-    # カテゴリーテーブルの作成
-    cursor.execute("""INSERT INTO main_categories (name) VALUES ('日常');""")
-    cursor.execute("""INSERT INTO main_categories (name) VALUES ('定期');""")
-    cursor.execute("""INSERT INTO main_categories (name) VALUES ('特別');""")
-    cursor.execute("""INSERT INTO main_categories (name) VALUES ('旅行');""")
+    """)
 
-    cursor.execute("""INSERT INTO sub_categories (main_category_id, name) VALUES (1, '二人で遊ぶお金');""") #1
-    cursor.execute("""INSERT INTO sub_categories (main_category_id, name) VALUES (1, '食費・消耗品');""") #2
-    cursor.execute("""INSERT INTO sub_categories (main_category_id, name) VALUES (1, '幸華お小遣い');""") #3
-    cursor.execute("""INSERT INTO sub_categories (main_category_id, name) VALUES (1, '大河お小遣い');""") #4
-    cursor.execute("""INSERT INTO sub_categories (main_category_id, name) VALUES (2, '大河給与');""") #5
-    cursor.execute("""INSERT INTO sub_categories (main_category_id, name) VALUES (2, '大河投資');""") #6
-    cursor.execute("""INSERT INTO sub_categories (main_category_id, name) VALUES (2, '幸華給与');""") #7
-    cursor.execute("""INSERT INTO sub_categories (main_category_id, name) VALUES (2, '幸華投資');""") #8
-    cursor.execute("""INSERT INTO sub_categories (main_category_id, name) VALUES (2, '家賃');""") #9
-    cursor.execute("""INSERT INTO sub_categories (main_category_id, name) VALUES (2, 'ガス代');""") #10
-    cursor.execute("""INSERT INTO sub_categories (main_category_id, name) VALUES (2, '電気代');""") #11
-    cursor.execute("""INSERT INTO sub_categories (main_category_id, name) VALUES (2, '水道代');""") #12
-    cursor.execute("""INSERT INTO sub_categories (main_category_id, name) VALUES (2, '通信代');""") #13
-    cursor.execute("""INSERT INTO sub_categories (main_category_id, name) VALUES (2, 'サブスクリプション');""") #14
-    cursor.execute("""INSERT INTO sub_categories (main_category_id, name) VALUES (3, '贈与');""") #15
-    cursor.execute("""INSERT INTO sub_categories (main_category_id, name) VALUES (3, '病院');""") #16
-    cursor.execute("""INSERT INTO sub_categories (main_category_id, name) VALUES (3, '引っ越し');""") #17
-    cursor.execute("""INSERT INTO sub_categories (main_category_id, name) VALUES (4, '202308イタリア');""") #18
-    cursor.execute("""INSERT INTO sub_categories (main_category_id, name) VALUES (4, '202312オーストラリア');""") #19
-    cursor.execute("""INSERT INTO sub_categories (main_category_id, name) VALUES (4, '202312宇治');""") #20
-    cursor.execute("""INSERT INTO sub_categories (main_category_id, name) VALUES (4, '202312大阪');""") #21
-    cursor.execute("""INSERT INTO sub_categories (main_category_id, name) VALUES (4, '202403三重');""") #22
-    cursor.execute("""INSERT INTO sub_categories (main_category_id, name) VALUES (4, '202408アメリカ');""") #23
-    cursor.execute("""INSERT INTO sub_categories (main_category_id, name) VALUES (4, '202407京都');""") #24
-    cursor.execute("""INSERT INTO sub_categories (main_category_id, name) VALUES (4, '202407京都');""") #25
-    cursor.execute("""INSERT INTO sub_categories (main_category_id, name) VALUES (3, 'イベント');""") #26
-    cursor.execute("""INSERT INTO sub_categories (main_category_id, name) VALUES (3, 'スイッチOTC');""") #27
+def insert_initial_categories(cursor):
+    categories = ['日常', '定期', '特別', '旅行']
+    cursor.executemany("INSERT INTO main_categories (name) VALUES (?);", [(cat,) for cat in categories])
 
-    input_categories = ["支出","収入","予算","定期契約","旅行","特別支出"]
+def insert_initial_sub_categories(cursor):
+    sub_categories = [
+        (1, '二人で遊ぶお金'), (1, '食費・消耗品'), (1, '幸華お小遣い'), (1, '大河お小遣い'),
+        (2, '大河給与'), (2, '大河投資'), (2, '幸華給与'), (2, '幸華投資'), (2, '家賃'),
+        (2, 'ガス代'), (2, '電気代'), (2, '水道代'), (2, '通信代'), (2, 'サブスクリプション'),
+        (3, '贈与'), (3, '病院'), (3, '引っ越し'), (4, '202308イタリア'), (4, '202312オーストラリア'),
+        (4, '202312宇治'), (4, '202312大阪'), (4, '202403三重'), (4, '202408アメリカ'), (4, '202407京都'),
+        (4, '202407京都'), (3, 'イベント'), (3, 'スイッチOTC')
+    ]
+    cursor.executemany("INSERT INTO sub_categories (main_category_id, name) VALUES (?, ?);", sub_categories)
+
+def insert_transactions_from_sheets(cursor, sh):
+    input_categories = ["支出", "収入", "予算", "定期契約", "旅行", "特別支出"]
     for input_category in input_categories:
-
         worksheet = sh.worksheet(input_category)
-
         data = worksheet.get_all_values()
         df = pd.DataFrame(data[1:], columns=data[0])
-        if input_category in ["支出", "定期契約", "特別支出", "旅行"]:
-            input_type = "支出"
-        elif input_category == "収入":
-            input_type = "収入"
-        elif input_category == "予算":
-            input_type = "予算"
-
+        input_type = get_input_type(input_category)
         for index, row in df.iterrows():
-            if row["カテゴリ"] == "食費/消耗品":
-                sub_category_no = 2
-            elif row["カテゴリ"] == "二人で遊ぶお金":
-                sub_category_no = 1
-            elif row["カテゴリ"] == "幸華お小遣い":
-                sub_category_no = 3
-            elif row["カテゴリ"] == "大河お小遣い":
-                sub_category_no = 4
-            elif row["カテゴリ"] == "耐久消耗品":
-                sub_category_no = 2
-            elif row["カテゴリ"] == "大河給与":
-                sub_category_no = 5
-            elif row["カテゴリ"] == "大河投資":
-                sub_category_no = 6
-            elif row["カテゴリ"] == "幸華給与":
-                sub_category_no = 7
-            elif row["カテゴリ"] == "幸華投資":
-                sub_category_no = 8
-            elif row["カテゴリ"] == "贈与":
-                sub_category_no = 15
-            elif row["カテゴリ"] == "電気代":
-                sub_category_no = 11
-            elif row["カテゴリ"] == "ガス代":
-                sub_category_no = 10
-            elif row["カテゴリ"] == "水道代":
-                sub_category_no = 12
-            elif row["カテゴリ"] == "通信代":
-                sub_category_no = 13
-            elif row["カテゴリ"] == "サブスク":
-                sub_category_no = 14
-            elif row["カテゴリ"] == "家賃":
-                sub_category_no = 9
-            elif row["カテゴリ"] == "その他":
-                if row["詳細"] == "UNHCR":
-                    sub_category_no = 14
-                else:
-                    sub_category_no = 12
-            elif row["カテゴリ"] == "イタリア":
-                sub_category_no = 18
-            elif row["カテゴリ"] == "アメリカ":
-                sub_category_no = 23
-            elif row["カテゴリ"] == "オーストラリア":
-                sub_category_no = 19
-            elif row["カテゴリ"] == "実家":
-                sub_category_no = 20
-            elif row["カテゴリ"] == "三重":
-                sub_category_no = 22
-            elif row["カテゴリ"] == "京都":
-                sub_category_no = 24
-            elif row["カテゴリ"] == "大阪帰省":
-                sub_category_no = 21
-            elif row["カテゴリ"] == "イベント":
-                sub_category_no = 26
-            elif row["カテゴリ"] == "贈与":
-                sub_category_no = 15
-            elif row["カテゴリ"] == "引っ越し":
-                sub_category_no = 17
-            elif row["カテゴリ"] == "病院":
-                sub_category_no = 16
-            else:
-                continue
-        
-            cursor.execute(
-                """
-                INSERT INTO transactions (sub_category_id, amount, type, date, detail)
-                VALUES (?, ?, ?, ?, ?);
-                """,
-                (sub_category_no, row[input_type],input_type,datetime.strptime(row['日付'],"%Y/%m/%d").strftime("%Y-%m-%d"), row['詳細'] if "詳細" in row else "")
-            )
+            sub_category_no = get_sub_category_no(row)
+            if sub_category_no:
+                cursor.execute("""
+                    INSERT INTO transactions (sub_category_id, amount, type, date, detail)
+                    VALUES (?, ?, ?, ?, ?);
+                """, (sub_category_no, row[input_type], input_type, datetime.strptime(row['日付'], "%Y/%m/%d").strftime("%Y-%m-%d"), row.get('詳細', "")))
 
-    conn.commit()
-    conn.close()
+def get_input_type(input_category):
+    if input_category in ["支出", "定期契約", "特別支出", "旅行"]:
+        return "支出"
+    elif input_category == "収入":
+        return "収入"
+    elif input_category == "予算":
+        return "予算"
 
-# Initialize data.
-if db_was_just_created:
-    initialize_data(conn)
+def get_sub_category_no(row):
+    category_map = {
+        "食費/消耗品": 2, "二人で遊ぶお金": 1, "幸華お小遣い": 3, "大河お小遣い": 4,
+        "耐久消耗品": 2, "大河給与": 5, "大河投資": 6, "幸華給与": 7, "幸華投資": 8,
+        "贈与": 15, "電気代": 11, "ガス代": 10, "水道代": 12, "通信代": 13, "サブスク": 14,
+        "家賃": 9, "その他": 12, "イタリア": 18, "アメリカ": 23, "オーストラリア": 19,
+        "実家": 20, "三重": 22, "京都": 24, "大阪帰省": 21, "イベント": 26, "引っ越し": 17,
+        "病院": 16
+    }
+    return category_map.get(row["カテゴリ"], None)
 
 def load_data(conn):
     """Loads the inventory data from the database."""
@@ -215,17 +131,12 @@ def load_data(conn):
         JOIN
             main_categories ON sub_categories.main_category_id = main_categories.id;
     """
-
     try:
         df = pd.read_sql(query, conn)
-        conn.close()
+        conn.close()   
     except:
         return None
-
     return df
-
-conn, db_was_just_created = connect_db()
-df = load_data(conn)
 
 def get_budget_and_spent(conn):
     """Fetches the budget and spent amounts for the current month in the '日常' category from the database."""
@@ -253,19 +164,7 @@ def get_budget_and_spent(conn):
     conn.close()
     return spent, budget
 
-conn, db_was_just_created = connect_db()
-spent, budget = get_budget_and_spent(conn)
-
-for category in budget.index:
-    spent_amount = spent.get(category, 0)
-    budget_amount = budget[category]
-    percentage = (spent_amount / budget_amount) * 100 if budget_amount > 0 else 0
-    st.write(f"{category}: {spent_amount}円 / {budget_amount}円")
-    st.progress(percentage / 100)
-
-# カテゴリとサブカテゴリの選択肢を取得
 def get_categories(conn):
-    
     cursor = conn.cursor()
     cursor.execute("SELECT id, name FROM main_categories")
     main_categories = cursor.fetchall()
@@ -273,100 +172,103 @@ def get_categories(conn):
     sub_categories = cursor.fetchall()
     return main_categories, sub_categories
 
-conn, db_was_just_created = connect_db()
+def update_data(df, changes):
+    try:
+
+        conn = sqlite3.connect(DB_FILENAME)  # 新しいデータベース接続を作成
+        cursor = conn.cursor()
+
+        if changes["edited_rows"]:
+            deltas = st.session_state.inventory_table["edited_rows"]
+            rows = [defaultdict(lambda: None, dict(df.iloc[i].to_dict(), **delta)) for i, delta in deltas.items()]
+            for row in rows:
+                if row['sub_category_id'] is None:
+                    row['sub_category_id'] = 1  # 適切なデフォルト値を設定
+            cursor.executemany("""
+                UPDATE transactions
+                SET
+                    sub_category_id = :sub_category_id,
+                    amount = :amount,
+                    type = :type,
+                    date = :date,
+                    detail = :detail
+                WHERE id = :id
+            """, rows)
+
+        if changes["added_rows"]:
+            rows = [defaultdict(lambda: None, row) for row in changes["added_rows"]]
+            for row in rows:
+                if row['sub_category_id'] is None:
+                    row['sub_category_id'] = 1  # 適切なデフォルト値を設定
+            cursor.executemany("""
+                INSERT INTO transactions
+                    (sub_category_id, amount, type, date, detail)
+                VALUES
+                    (:sub_category_id, :amount, :type, :date, :detail)
+            """, rows)
+
+        if changes["deleted_rows"]:
+            cursor.executemany("DELETE FROM transactions WHERE id = :id", ({"id": int(df.loc[i, "id"])} for i in changes["deleted_rows"]))
+
+            conn.commit()
+            conn.close()
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
+
+# Main script
+sh = get_worksheet_from_gspread_client()
+if not exists_db_file():
+    conn = connect_db()
+    initialize_data(conn, sh)
+
+conn = connect_db()
+spent, budget = get_budget_and_spent(conn)
+
+st.title("データを編集")
+for category in budget.index:
+    spent_amount = spent.get(category, 0)
+    budget_amount = budget[category]
+    percentage = (spent_amount / budget_amount) * 100 if budget_amount > 0 else 0
+    st.write(f"{category}: {spent_amount}円 / {budget_amount}円")
+    st.progress(percentage / 100)
+
+conn = connect_db()
 main_categories, sub_categories = get_categories(conn)
 
 # Streamlit UI
 st.title("データベースにデータを追加")
-
-# 日付入力
 date = st.date_input("日付", datetime.now())
-
-# カテゴリ選択
 main_category = st.selectbox("カテゴリ", [cat[1] for cat in main_categories])
 main_category_id = next(cat[0] for cat in main_categories if cat[1] == main_category)
-
-# サブカテゴリ選択
 sub_category = st.selectbox("サブカテゴリ", [sub[2] for sub in sub_categories if sub[1] == main_category_id])
 sub_category_id = next(sub[0] for sub in sub_categories if sub[2] == sub_category)
-
-# 詳細入力
+input_type = st.selectbox("種別", ["支出", "収入", "予算"])
 detail = st.text_input("詳細")
-
-# 支出額入力
 expense = st.number_input("支出額", min_value=0)
 
-
-def update_data(conn, df, changes):
-    """Updates the inventory data in the database."""
+if st.button("データを追加"):
+    conn = connect_db()
     cursor = conn.cursor()
-
-    if changes["edited_rows"]:
-        deltas = st.session_state.inventory_table["edited_rows"]
-        rows = []
-
-        for i, delta in deltas.items():
-            row_dict = df.iloc[i].to_dict()
-            row_dict.update(delta)
-            rows.append(row_dict)
-
-        cursor.executemany(
-            """
-            UPDATE inventory
-            SET
-                item_name = :item_name,
-                price = :price,
-                units_sold = :units_sold,
-                units_left = :units_left,
-                cost_price = :cost_price,
-                reorder_point = :reorder_point,
-                description = :description
-            WHERE id = :id
-            """,
-            rows,
-        )
-
-    if changes["added_rows"]:
-        cursor.executemany(
-            """
-            INSERT INTO inventory
-                (id, item_name, price, units_sold, units_left, cost_price, reorder_point, description)
-            VALUES
-                (:id, :item_name, :price, :units_sold, :units_left, :cost_price, :reorder_point, :description)
-            """,
-            (defaultdict(lambda: None, row) for row in changes["added_rows"]),
-        )
-
-    if changes["deleted_rows"]:
-        cursor.executemany(
-            "DELETE FROM inventory WHERE id = :id",
-            ({"id": int(df.loc[i, "id"])} for i in changes["deleted_rows"]),
-        )
-
+    cursor.execute("""
+        INSERT INTO transactions (sub_category_id, amount, type, date, detail)
+        VALUES (?, ?, ?, ?, ?);
+    """, (sub_category_id, expense, input_type, date.strftime("%Y-%m-%d"), detail))
     conn.commit()
 
-
-# Load data from database
+conn = connect_db()
 df = load_data(conn)
-# 正規表現パターンを定義
-date_pattern = re.compile(r"^\d{4}-\d{2}-\d{2}$")
+
+st.title("データを編集")
 
 edited_df = st.data_editor(
     df,
-    disabled=["id"],  # Don't allow editing the 'id' column.
-    num_rows="dynamic",  # Allow appending/deleting rows.
+    disabled=["id"],
+    num_rows="dynamic",
     column_config={
-        # Show dollar sign before price columns.
         "amount": st.column_config.NumberColumn(format="¥%f"),
-        # "date": st.column_config.TextColumn(
-        #     validate=lambda x: bool(date_pattern.match(x)),
-        #     help="YYYY-MM-DD形式で入力してください"
-        # )
     },
     key="inventory_table",
 )
-
-
 
 has_uncommitted_changes = any(len(v) for v in st.session_state.inventory_table.values())
 
@@ -374,7 +276,6 @@ st.button(
     "Commit changes",
     type="primary",
     disabled=not has_uncommitted_changes,
-    # Update data in database
     on_click=update_data,
-    args=(conn, df, st.session_state.inventory_table),
+    args=(df, st.session_state.inventory_table),
 )
