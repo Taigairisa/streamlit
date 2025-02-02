e28b663cbf5b828e08fd1cefd87e51a8b83b4509
taigairisa <iri.taiga@gmail.com>
2025-02-02 16:17:43 +0900

    つｔｔ

--- db_test.py
+++ db_test.py
@@ -299,6 +299,9 @@ def backup_data_to_spreadsheet(conn):
 
     if (not backup_time) or datetime.now(pytz.timezone('Asia/Tokyo')) - datetime.strptime(backup_time[1], "%Y/%m/%d %H:%M:%S") >= timedelta(days=1):
         cursor.execute("INSERT INTO backup_time (time) VALUES (?)", [datetime.now(pytz.timezone('Asia/Tokyo')).strftime("%Y/%m/%d %H:%M:%S")])
+        backup_time = cursor.execute("SELECT * FROM backup_time ORDER BY time DESC LIMIT 1").fetchone()
+        st.write(backup_time)
+
         sh = get_worksheet_from_gspread_client()
         tables = ["main_categories", "sub_categories", "transactions", "backup_time"]
         for table in tables:
