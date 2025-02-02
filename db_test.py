a2c845e9a9c14f7c737a6f6c31dc64e5add1a6c0
taigairisa <iri.taiga@gmail.com>
2025-02-02 16:07:47 +0900

    てすと

--- db_test.py
+++ db_test.py
@@ -295,7 +295,7 @@ def backup_data_to_spreadsheet(conn):
     # cursor.execute("CREATE TABLE IF NOT EXISTS backup_time (id INTEGER PRIMARY KEY AUTOINCREMENT, time TEXT);")
     backup_time = cursor.execute("SELECT * FROM backup_time ")
     backup_time_str = backup_time.fetchall() #ORDER BY time DESC LIMIT 1
-    st.write(backup_time)
+    st.write(backup_time_str)
 
     if (not backup_time) or datetime.now(pytz.timezone('Asia/Tokyo')) - datetime.strptime(backup_time[1], "%Y/%m/%d %H:%M:%S") >= timedelta(days=1):
         cursor.execute("INSERT INTO backup_time (time) VALUES (?)", [datetime.now(pytz.timezone('Asia/Tokyo')).strftime("%Y/%m/%d %H:%M:%S")])
