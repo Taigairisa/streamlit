52bbf27f9062014889f7d99c81118ffb63b43879
taigairisa <iri.taiga@gmail.com>
2025-02-02 16:30:31 +0900

    debug

--- db_test.py
+++ db_test.py
@@ -298,6 +298,7 @@ def backup_data_to_spreadsheet(conn):
     st.write(backup_time[1])
     st.write(datetime.strptime(backup_time[1], "%Y/%m/%d %H:%M:%S"))
 
+    st.write(datetime.now(pytz.timezone('Asia/Tokyo')))
     if (not backup_time) or (datetime.now(pytz.timezone('Asia/Tokyo')) - datetime.strptime(backup_time[1], "%Y/%m/%d %H:%M:%S") >= timedelta(days=1)):
         cursor.execute("INSERT INTO backup_time (time) VALUES (?)", [datetime.now(pytz.timezone('Asia/Tokyo')).strftime("%Y/%m/%d %H:%M:%S")])
         conn.commit()
