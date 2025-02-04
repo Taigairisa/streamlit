7f40b1a3d163b2ef69d278dd43ae5954dc88e776
taigairisa <iri.taiga@gmail.com>
2025-02-04 23:28:35 +0900

    1分ごとの同期

--- db_test.py
+++ db_test.py
@@ -302,7 +302,7 @@ main_category_id = next(cat[0] for cat in main_categories if cat[1] == main_cate
 backup_time = conn.cursor().execute("SELECT * FROM backup_time ORDER BY time DESC LIMIT 1").fetchone()
 now_date = datetime.strptime(datetime.now(pytz.timezone('Asia/Tokyo')).strftime("%Y/%m/%d %H:%M:%S"), "%Y/%m/%d %H:%M:%S") 
 
-if (not backup_time) or (now_date - datetime.strptime(backup_time[1], "%Y/%m/%d %H:%M:%S") >= timedelta(days=1)):
+if (not backup_time) or (now_date - datetime.strptime(backup_time[1], "%Y/%m/%d %H:%M:%S") >= timedelta(minutes=1)):
     st.warning("バックアップを実行します")
     backup_data_to_spreadsheet(conn)
 
